b3a9c95365028eba57aa5d7b0d6735aae2262900
hhas0015 <69443727+hhas0015@users.noreply.github.com>
2020-08-24 20:17:37 +1000

    Add project files.

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using FIT5032_W04_CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FIT5032_W04_CodeFirst.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_W04_CodeFirst.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
